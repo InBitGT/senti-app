2e0285129cc4cfbc4c16a5abc6c27468f118ed5a
asofimp <asofimp@gmail.com>
2026-03-31 23:36:21 -0600

    feat: structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,20 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
+import { SplashScreen, Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { LogBox } from 'react-native';
+import 'react-native-reanimated';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+LogBox.ignoreAllLogs();
 
-LogBox.ignoreAllLogs()
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <GluestackUIProvider mode="dark">
